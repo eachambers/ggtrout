0c307fe416786f6a8b4589fb282ae95ba2af66a3
E. Anne Chambers <eachambers@utexas.edu>
2023-09-04 11:44:49 -0600

    add package

---
+++ R/palettes.R
@@ -0,0 +1,7 @@
+ggtrout_palettes <- list(
+  brook1 = list(c("#df3d3a", "#d9bb01", "#e0902e", "#92a1c6", "#757877", "#563c2a", "#babfb4"), colorblind = TRUE, type = "qualitative"),
+  brook2 = list(c("#ac7d0d", "#bd9500", "#d9bb01", "#e8ce63", "#edd68b"), colorblind = TRUE, type = "monochromatic"),
+  brook3 = list(c("#ac7d0d", "#bd9500", "#d9bb01", "#e8ce63", "#ffd1b3", "#f3a567", "#e68114", "#eb6036", "#df3d3a"), colorblind = TRUE, type = "diverging"),
+  cutthroat1 = list(c("#"), colorbind = TRUE, type = "monochromatic"),
+  rainbow1 = c("#")
+)
