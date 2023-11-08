35fad0f43bfc21c2ab5ea410e55f3cb08b6b0097
E. Anne Chambers <eachambers@utexas.edu>
2023-11-08 11:42:02 -0700

    update misc

--- R/palettes.R
+++ R/palettes.R
@@ -88,11 +88,11 @@ print.palette <- function(x, ...) {
   old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
   on.exit(par(old))
 
-  image(1:n, 1, as.matrix(1:n), col = x,
+  graphics::image(1:n, 1, as.matrix(1:n), col = x,
         ylab = "", xaxt = "n", yaxt = "n", bty = "n")
 
-  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
-  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1.5)
+  graphics::rect(0, 0.9, n + 1, 1.1, col = grDevices::rgb(1, 1, 1, 0.8), border = NA)
+  graphics::text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1.5)
 }
 
 #' Check whether ggtrout palettes are colorblind friendly
